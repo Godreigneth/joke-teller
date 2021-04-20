57355b4e302dd0f5dc0bdb517213dc74c87fa6dc
Godreigneth <godreignethlevidibia@yahoo.com>
2021-04-20 20:30:49 +0000

    Applied the VoiceRSS Speach Function

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const audioElement = document.getElementById('audio');
 function toggleButton() {
   button.disabled = !button.disabled;
 }
+
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, '%20');
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    key: 'YOUR_API_KEY_HERE',
+    src: jokeString,
+    hl: 'en-us',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
