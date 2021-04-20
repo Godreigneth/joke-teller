472d688ca68a69b09afcd4dfeb58b31233e94d26
Godreigneth <godreignethlevidibia@yahoo.com>
2021-04-20 20:29:32 +0000

    Added the Disable/Enable Button function

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
